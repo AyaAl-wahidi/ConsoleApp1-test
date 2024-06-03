5ba27fda56ab4ad516ba792aa5016b28945448e9
Aya <ayanalwahidi@gmail.com>
2024-06-03 11:13:05 +0300

    branch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, All!");
+            Console.WriteLine("Hello, test branch!");
         }
     }
 }
